04df033a995347539d3039c485e3ce0244e328e0
mortalshot <seregagl1996@gmail.com>
2025-01-31 16:44:58 +0300

    v4.1

--- src/js/app.js
+++ src/js/app.js
@@ -208,7 +208,7 @@ import * as flsScroll from "./files/scroll/scroll.js";
 
 // Плавная навигация по странице
 // Документация: https://start-template.ru/modul-prokrutka-k-nuzhnomu-bloku-plavnaya-navigacziya-po-stranicze-chertogi-frilansera-v3-0-0/
-// flsScroll.pageNavigation();
+flsScroll.pageNavigation();
 
 // Функциональность добавления классов в хеддер при прокрутке
 // Документация: https://start-template.ru/modul-dobavleniya-klassov-v-shapku-pri-prokrutke-straniczy-chertogi-frilansera-v3-0-0/
@@ -227,7 +227,7 @@ import * as flsScroll from "./files/scroll/scroll.js";
 Документация плагина: https://www.lightgalleryjs.com/docs/
 Сниппет(HTML):
 */
-// import "./files/gallery.js";
+import "./files/gallery.js";
 
 // ========================================================================================================================================================================================================================================================
 // Масонри сетка ========================================================================================================================================================================================================================================================
